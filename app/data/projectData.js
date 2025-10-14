d9c6eb2d85adeba996d94fcf3d10b4316564c3cf
sreekumar <sreekumar.d@technoduces.com>
2025-10-14 17:47:44 +0530

    project added

--- app/data/projectData.js
+++ app/data/projectData.js
@@ -1,47 +1,60 @@
 const projectsData = [
-      {
+  {
     id: 1,
-    title: "Lakshmi Krishna Naturals",
-    image: "/lakshmikrishna.webp",
-    description: "E-commerce site for personal care products with seamless browsing and checkout. Built using React.js, Next.js, and Tailwind CSS.",
-    link: "https://lakshmikrishnanaturals.com/",
+    title: 'Lakshmi Krishna Naturals',
+    image: '/lakshmikrishna.webp',
+    description:
+      'E-commerce site for personal care products with seamless browsing and checkout. Built using React.js, Next.js, and Tailwind CSS.',
+    link: 'https://lakshmikrishnanaturals.com/',
   },
   {
     id: 2,
-    title: "CRM - Zoho Timesheet Clone",
-    image: "/timesheets-crm.png",
-    description: "Custom CRM to track daily work logs, tasks, and reports efficiently. Built using Next.js and Bootstrap.",
-    link: "https://pms.technotackle.in/",
+    title: 'CRM - Zoho Timesheet Clone',
+    image: '/timesheets-crm.png',
+    description:
+      'Custom CRM to track daily work logs, tasks, and reports efficiently. Built using Next.js and Bootstrap.',
+    link: 'https://pms.technotackle.in/',
   },
   {
     id: 3,
-    title: "Sarvam Logistics",
-    image: "/sarvam.webp",
-    description: "Logistics management system with real-time tracking and air cargo support. Built using HTML, CSS, JavaScript, jQuery, and Material-UI.",
-    link: "https://www.sarvamlogistics.com/",
+    title: 'Sarvam Logistics',
+    image: '/sarvam.webp',
+    description:
+      'Logistics management system with real-time tracking and air cargo support. Built using HTML, CSS, JavaScript, jQuery, and Material-UI.',
+    link: 'https://www.sarvamlogistics.com/',
   },
-   {
+
+  {
     id: 4,
-    title: "Soco Uniforms",
-    image: "/soco.jpg",
-    description: "E-commerce platform for school uniforms with secure payments and smooth order management. Built using React.js, Next.js, and Material-UI.",
-    link: "https://theskoolstore.com/",
+    title: 'Stratview Research',
+    image: '/startview.png',
+    description:
+      'A market research and consulting platform offering industry insights and reports. Built with a modern, responsive front-end design.',
+    link: 'https://stratviewresearch.com/',
   },
   {
     id: 5,
-    title: "Franchise Portal",
-    image: "/1851.jpg",
+    title: 'Franchise Portal',
+    image: '/1851.jpg',
     description:
-      "Internal platform for franchise managers and head office staff to manage administrative workflows. Built using Next.js and Material-UI.",
-    link: "https://1851franchise.com/",
+      'Internal platform for franchise managers and head office staff to manage administrative workflows. Built using Next.js and Material-UI.',
+    link: 'https://1851franchise.com/',
   },
   {
     id: 6,
-    title: "Effitrac Product",
-    image: "/effitrac.svg",
+    title: 'Soco Uniforms',
+    image: '/soco.jpg',
     description:
-      "ERP-based business solution for inventory, finance, and workflow automation. Built using React.js and Material-UI.",
-    link: "https://your-effitrac-link.com",
+      'E-commerce platform for school uniforms with secure payments and smooth order management. Built using React.js, Next.js, and Material-UI.',
+    link: 'https://theskoolstore.com/',
+  },
+  {
+    id: 7,
+    title: 'Effitrac Product',
+    image: '/effitrac.svg',
+    description:
+      'ERP-based business solution for inventory, finance, and workflow automation. Built using React.js and Material-UI.',
+    link: 'https://your-effitrac-link.com',
   },
 ];
 
